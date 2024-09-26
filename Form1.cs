764e462cf87f8e64c36e4954e62d79f1bc31105d
pbkx <nanomantis07@gmail.com>
2024-09-26 14:29:46 -0500

    Basic wireframe framework for the recipe organizer.

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,40 @@ namespace RecipeOrganizer
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
