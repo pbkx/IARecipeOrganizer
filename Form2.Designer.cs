ae50a5f82d00c0050b55afea7b7bbc995b7121b4
Jason2Car <Jasonshuncheng@gmail.com>
2024-09-30 14:23:27 -0500

    Edited add recipe form

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Namelbl = new System.Windows.Forms.Label();
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.Ingredientstxt = new System.Windows.Forms.TextBox();
+            this.Ingredientslbl = new System.Windows.Forms.Label();
+            this.EnterIngredbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Namelbl
+            // 
+            this.Namelbl.AutoSize = true;
+            this.Namelbl.Location = new System.Drawing.Point(256, 117);
+            this.Namelbl.Name = "Namelbl";
+            this.Namelbl.Size = new System.Drawing.Size(75, 13);
+            this.Namelbl.TabIndex = 0;
+            this.Namelbl.Text = "Recipe Name:";
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.Location = new System.Drawing.Point(353, 114);
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(100, 20);
+            this.Nametxt.TabIndex = 1;
+            // 
+            // Ingredientstxt
+            // 
+            this.Ingredientstxt.Location = new System.Drawing.Point(353, 161);
+            this.Ingredientstxt.Name = "Ingredientstxt";
+            this.Ingredientstxt.Size = new System.Drawing.Size(100, 20);
+            this.Ingredientstxt.TabIndex = 3;
+            // 
+            // Ingredientslbl
+            // 
+            this.Ingredientslbl.AutoSize = true;
+            this.Ingredientslbl.Location = new System.Drawing.Point(256, 164);
+            this.Ingredientslbl.Name = "Ingredientslbl";
+            this.Ingredientslbl.Size = new System.Drawing.Size(62, 13);
+            this.Ingredientslbl.TabIndex = 2;
+            this.Ingredientslbl.Text = "Ingredients:";
+            // 
+            // EnterIngredbtn
+            // 
+            this.EnterIngredbtn.Location = new System.Drawing.Point(482, 164);
+            this.EnterIngredbtn.Name = "EnterIngredbtn";
+            this.EnterIngredbtn.Size = new System.Drawing.Size(75, 23);
+            this.EnterIngredbtn.TabIndex = 4;
+            this.EnterIngredbtn.Text = "button1";
+            this.EnterIngredbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.Controls.Add(this.EnterIngredbtn);
+            this.Controls.Add(this.Ingredientstxt);
+            this.Controls.Add(this.Ingredientslbl);
+            this.Controls.Add(this.Nametxt);
+            this.Controls.Add(this.Namelbl);
+            this.Name = "Form2";
+            this.Text = "Enter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Namelbl;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.TextBox Ingredientstxt;
+        private System.Windows.Forms.Label Ingredientslbl;
+        private System.Windows.Forms.Button EnterIngredbtn;
     }
 }
